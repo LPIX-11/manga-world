413ba18da6130a3b37600b10fbb5b363f9cd7265
Mohamed Johnson <mohamedjohnson31@gmail.com>
2020-05-03 17:34:46 +0000

    :wrench: New Test Images Added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,93 +17,93 @@ export default function HomeScreen() {
           <TopMangaCard
             mangaTitle={'Kimetsu No Yaiba'}
             mangaImage={require('../assets/images/mangas/cover/kimetsu.jpg')}
-            readers={ '1,926' }
+            readers={'1,926'}
           />
           <TopMangaCard
             mangaTitle={'Sword Art Online'}
             mangaImage={require('../assets/images/mangas/cover/sao.jpg')}
-            readers={ '1,101' }
+            readers={'1,101'}
           />
           <TopMangaCard
             mangaTitle={'Akame ga Kill!'}
             mangaImage={require('../assets/images/mangas/cover/akame.jpg')}
-            readers={ '1,001' }
+            readers={'1,001'}
           />
           <TopMangaCard
             mangaTitle={'The Gamer'}
             mangaImage={require('../assets/images/mangas/cover/thegamer.jpg')}
-            readers={ '801' }
+            readers={'801'}
           />
         </CardSilder>
       </TopMangas>
 
       <RowWrapper>
-          <MangaCard
-            mangaTitle={'The Gamer'}
-            mangaImage={require('../assets/images/mangas/cover/thegamer.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'The Gamer'}
-            mangaImage={require('../assets/images/mangas/cover/sao.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'The Gamer'}
-            mangaImage={require('../assets/images/mangas/cover/akame.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'The Gamer'}
-            mangaImage={require('../assets/images/mangas/cover/kimetsu.jpg')}
-            readers={ '801' }
-          />
+        <MangaCard
+          mangaTitle={'The Gamer'}
+          mangaImage={require('../assets/images/mangas/cover/asterix.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'The Gamer'}
+          mangaImage={require('../assets/images/mangas/cover/chivalry.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'The Gamer'}
+          mangaImage={require('../assets/images/mangas/cover/iiw.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'The Gamer'}
+          mangaImage={require('../assets/images/mangas/cover/bokunohero.jpg')}
+          readers={'801'}
+        />
       </RowWrapper>
 
       <RowWrapper>
-          <MangaCard
-            mangaTitle={'Guilty Crown V1'}
-            mangaImage={require('../assets/images/mangas/cover/guiltyv1.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'Fate Stay Night Unlimited BladeWorks'}
-            mangaImage={require('../assets/images/mangas/cover/fatestay.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'Guilty Crown V2'}
-            mangaImage={require('../assets/images/mangas/cover/guiltyv2.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'The Gamer'}
-            mangaImage={require('../assets/images/mangas/cover/kimetsu.jpg')}
-            readers={ '801' }
-          />
+        <MangaCard
+          mangaTitle={'The Gamer'}
+          mangaImage={require('../assets/images/mangas/cover/kimetsu.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'Guilty Crown V1'}
+          mangaImage={require('../assets/images/mangas/cover/guiltyv1.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'Fate Stay Night Unlimited BladeWorks'}
+          mangaImage={require('../assets/images/mangas/cover/fatestay.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'Guilty Crown V2'}
+          mangaImage={require('../assets/images/mangas/cover/guiltyv2.jpg')}
+          readers={'801'}
+        />
       </RowWrapper>
 
       <RowWrapper>
-          <MangaCard
-            mangaTitle={'Sirius The Jaeger'}
-            mangaImage={require('../assets/images/mangas/cover/sirius.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'The Gamer'}
-            mangaImage={require('../assets/images/mangas/cover/sao.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'The Gamer'}
-            mangaImage={require('../assets/images/mangas/cover/akame.jpg')}
-            readers={ '801' }
-          />
-          <MangaCard
-            mangaTitle={'The Gamer'}
-            mangaImage={require('../assets/images/mangas/cover/kimetsu.jpg')}
-            readers={ '801' }
-          />
+        <MangaCard
+          mangaTitle={'Sirius The Jaeger'}
+          mangaImage={require('../assets/images/mangas/cover/tokyo.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'The Gamer'}
+          mangaImage={require('../assets/images/mangas/cover/sao.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'The Gamer'}
+          mangaImage={require('../assets/images/mangas/cover/akame.jpg')}
+          readers={'801'}
+        />
+        <MangaCard
+          mangaTitle={'The Gamer'}
+          mangaImage={require('../assets/images/mangas/cover/kimetsu.jpg')}
+          readers={'801'}
+        />
       </RowWrapper>
     </Wrapper>
   );
